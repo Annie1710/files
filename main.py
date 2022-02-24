6f931120f8260f1025b8ebb716516665e0b46a01
Annie <91362039+Annie1710@users.noreply.github.com>
2022-02-24 09:30:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ __human_name__ = "files"
 import os
 import shutil
 from zipfile import ZipFile
-import re
 
 
 def clean_cache():
-    cwd = os. getcwd()
+    cwd = os.getcwd()
+    print(cwd)
     if os.path.exists('cache'):
         shutil.rmtree("cache")
     os.mkdir('cache')
@@ -21,28 +21,30 @@ def cache_zip(zip_file_path, cache_dir_path):  # (source, target)
 
 
 def cached_files():
-    path = "f:\\WINC\\Opdrachten DAWP\\files\\cache"
-    dir_list = os.listdir(path)
-    print(dir_list)
+    cwd = os.getcwd()
+    ab_path = os.path.abspath(cwd)
+    path = os.path.join(ab_path, "cache")
+    dir_list = []
+    for file in os.listdir(path):
+        dir_list = dir_list + [os.path.join(path, file)]
     return dir_list
-
-
-dir_list = cached_files()
 
 
 def find_password(dir_list):
     for file in dir_list:
-        with open(file):
-            if "password" in open(file).read():
-                text = file.read()
-                x = text.find("password")
-                password = text[x:]
-            print(password)
+        content = open(file)
+        string = content.read()
+        if 'password' in string:
+            index = string.find('password')
+            l_word = len("password")
+            password = string[index+l_word+2:index+38]
+            print(string[index+l_word+2:index+38])
             return password
-               
+
 
 if __name__ == "__main__":
     clean_cache()
     cache_zip("./data.zip", "./cache")
     cached_files()
+    dir_list = cached_files()
     find_password(dir_list)
